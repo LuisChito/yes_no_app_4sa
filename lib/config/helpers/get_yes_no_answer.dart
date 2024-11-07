0a8fbaa7d42216165ecc4c532438a83adbd50a06
Chito <lssalazarcanche1@gmail.com>
2024-11-07 17:49:53 -0600

    Ajustando Provider

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -1,20 +1,15 @@
+import 'package:dio/dio.dart';
 import 'package:yes_no_app_4sa/domain/entities/message.dart';
 
 class GetYesNoAnswer {
-
-  //Se crea una instancia (objeto) de la clase Dio
-  //Instanciar es hacer referencia a _____
-
+  // Se crea instancia(objeto) de la clase Dio para manejar las peticiones HTTP
   final _dio = Dio();
 
-
-  //Obtener respuesta
-
-  Future <Message> getAnswer () async{
-    //Almacenar la petición en una variable
+  // Obtener la respuesta
+  Future<Message> getAnswer() async {
     final response = await _dio.get('https://yesno.wtf/api');
 
-    //Generar el error
+    // Provocar o generar un error
     throw UnimplementedError();
   }
 }
