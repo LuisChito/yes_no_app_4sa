0215a49f4d6776c5da4076741c2f3bf2af7b6f6c
Chito <lssalazarcanche1@gmail.com>
2024-11-11 10:22:17 -0600

    Starting Model

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -1,15 +1,18 @@
 import 'package:dio/dio.dart';
 import 'package:yes_no_app_4sa/domain/entities/message.dart';
+import 'package:yes_no_app_4sa/infraestructure/models/yes_no_model.dart';
 
 class GetYesNoAnswer {
-  // Se crea instancia(objeto) de la clase Dio para manejar las peticiones HTTP
+  //Se crea instancia(objeto) de la clase Dio
+  //Para manejar las peticiones HTTP
   final _dio = Dio();
 
-  // Obtener la respuesta
+  //Obtener la respuesta
   Future<Message> getAnswer() async {
+    //almacenar la petición GET en una variable
     final response = await _dio.get('https://yesno.wtf/api');
+    final yesNoModel = YesNoModel.fromJsonMap(response.data);
+    return yesNoModel.toMessageEntity();
 
-    // Provocar o generar un error
-    throw UnimplementedError();
   }
 }
--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,40 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app_4sa/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app_4sa/domain/entities/message.dart';
 
-//Puntos para luis, Ader, Pablo, Daniel Chalé, Kevin
-
-class ChatProvider extends ChangeNotifier {
+class ChatProvider extends ChangeNotifier{
+  
   List<Message> messageList = [
-    Message(text: 'Hola Ader', fromWho: FromWho.me),
-    Message(text: '¿Lloras porque reprobaste tópicos?', fromWho: FromWho.me)
-  ];
-  //Controlador para manejar la posición del scroll
+    Message(text: 'Hola tilin', fromWho: FromWho.me),
+    Message(text: 'lloras porque reprobaste TAP?', fromWho: FromWho.me)
+ ];
+  //Controlador para manerar la posición del scroll
   final ScrollController chatScrollController = ScrollController();
+  //Instancia de la clase GetYesNoAnswer
+  final getYesNoAnswer = GetYesNoAnswer();
 
   //Enviar un mensaje
   Future<void> sendMessage(String text) async {
-    //El mensaje siempre va a ser "me" porque yo lo envío
+    //Evita que se mande mensaje vacio
+      if ( text.isEmpty) return;
+    //El mensaje siempre va a ser 'me' porque yo lo envío
     final newMessage = Message(text: text, fromWho: FromWho.me);
-    //Agrega un elemento a lista "messageList"
+    //Agrega un elemento a la lista "messageList"
     messageList.add(newMessage);
-    //Notifica si algo de provider cambió para que se guarde en el estado
+    //Notifica si algo de provider cambió para que se guarde en el
+
+    if (text.endsWith('?')){
+      herReply();
+    }
     notifyListeners();
-    //Mueve el scroll
+    //Mueve el Scroll
+    moveScrollToBottom();
+  }
+//Future es una promesa
+//get es para que el servidor me diga/obtenga algo
+//dio es un paquete de flutter para manejar peticiones http
+
+  Future<void> herReply() async{
+    //Obtener el mensaje de la petición
+    final herMessage = await getYesNoAnswer.getAnswer();
+    //añadir el mensaje de mi crush a la lista
+    messageList.add(herMessage);
+    //Notifica si algo de provider cambió para el estado
+    notifyListeners();
+    //Mueve el scroll hasta el ultimo mensaje
     moveScrollToBottom();
   }
 
-  //Mover el scroll al último mensaje
-  Future<void> moveScrollToBottom() async {
-    //Un pequeño atraso en la animación para garantizar que siempre
-    //se verá aún cuando se envíen mensajes cortos y rápidos
-    await Future.delayed(const Duration(seconds: 1));
-    chatScrollController.animateTo(
-        //Offset: posición de la animación
-        //maxScrollExtent determina a lo máximo que el scroll puede dar
-        chatScrollController.position.maxScrollExtent,
-        //Duración de la animación
-        duration: const Duration(milliseconds: 300),
-        //"Rebote" al final de la animación
-        curve: Curves.easeOut);
+
+//Mover el scroll al ultimo mensaje
+Future<void> moveScrollToBottom() async {
+  // Verificar si el ScrollController tiene clientes antes de hacer scroll
+  if (chatScrollController.hasClients) {
+    //Un pequeño atraso en la animación para garantizar que siempre se verá aun cuando se envien
+    //mensajes cortos y rápidps
+    await chatScrollController.animateTo(
+      //maxScrollExtent: determina a lo máximo que el scroll puede dar
+      chatScrollController.position.maxScrollExtent, 
+      duration: const Duration(milliseconds: 300), 
+      curve: Curves.easeOut,
+      );
+    }  
   }
 }
